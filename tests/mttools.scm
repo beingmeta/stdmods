9432952ded1bf35f203922d533b9f51c3940f3aa
Ken Haase <kh@beingmeta.com>
2020-07-12 16:16:16 -0400

    Make mttools test more generic

--- tests/mttools.scm
+++ tests/mttools.scm
@@ -9,10 +9,10 @@
 
 (config! 'bricosource "/data/bg/brico")
 (config! 'cachelevel 2)
-(use-module '{brico mttools})
+(use-module '{mttools })
 (define all-slots (make-hashset))
 
-(define (test-mttools)
-  (do-choices-mt (f (pool-elts brico-pool) 4 8192 mt/fetchoids)
+(define (test-mttools pool)
+  (do-choices-mt (f (pool-elts pool) 4 8192 mt/fetchoids)
      (hashset-add! all-slots (getslots f))))
 
